1b14bd411a01784c292234833fae64e99adb862e
Rob Nelson <robert.nelson@duke.edu>
2019-01-16 13:12:23 -0500

    #27: remove elixir graphql endpoint

--- experiments/duke/graphql_endpoint/lib/graphql_endpoint/search.ex
+++
@@ -1,9 +0,0 @@
-defmodule GraphqlEndpoint.Search do
-  def fetch(index, types, query) do
-    Elastix.Search.search(endpoint(), index, types, query)
-  end
-
-  def endpoint do
-    Application.get_env(:graphql_endpoint, :endpoint)
-  end
-end
