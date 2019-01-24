7560906ba306b53fef236b153ef9c2f92cd91b43
Rob Nelson <robert.nelson@duke.edu>
2019-01-24 15:21:42 -0500

    #29: go-fmt testing file

--- experiments/duke/graphql_endpoint/paging_test.go
+++ experiments/duke/graphql_endpoint/paging_test.go
@@ -1,18 +1,18 @@
 package graphql_endpoint_test
 
 import (
-   "testing"
-   "fmt"
-   "github.com/OIT-ads-web/graphql_endpoint"
+	"fmt"
+	"github.com/OIT-ads-web/graphql_endpoint"
+	"testing"
 )
 
 func TestPagingPerPage(t *testing.T) {
 	var start = 1
 	var perPage = 10
 	var total = 44
-	
+
 	pageInfo1 := graphql_endpoint.FigurePaging(start, perPage, total)
-	if (pageInfo1.PerPage != 10) {
+	if pageInfo1.PerPage != 10 {
 		t.Error(fmt.Printf("should be page 10 per page if perPage = %d\n", perPage))
 	}
 }
@@ -21,21 +21,20 @@ func TestPagingTotal(t *testing.T) {
 	var start = 1
 	var perPage = 10
 	var total = 44
-	
+
 	pageInfo1 := graphql_endpoint.FigurePaging(start, perPage, total)
-	if (pageInfo1.TotalPages != 5) {
+	if pageInfo1.TotalPages != 5 {
 		t.Error(fmt.Printf("should be page 5 pages with size=%d and count=%d\n", perPage, total))
 	}
-
 }
 
 func TestPagingStart(t *testing.T) {
 	var start = 1
 	var perPage = 10
 	var total = 44
-	
+
 	pageInfo1 := graphql_endpoint.FigurePaging(start, perPage, total)
-	if (pageInfo1.CurrentPage != 1) {
+	if pageInfo1.CurrentPage != 1 {
 		t.Error(fmt.Printf("should be page 1 if start = %d and perPage = %d", start, perPage))
 	}
 }
@@ -44,9 +43,9 @@ func TestPagingSecond(t *testing.T) {
 	var start = 14
 	var perPage = 10
 	var total = 44
-	
+
 	pageInfo1 := graphql_endpoint.FigurePaging(start, perPage, total)
-	if (pageInfo1.CurrentPage != 2) {
+	if pageInfo1.CurrentPage != 2 {
 		t.Error(fmt.Printf("should be page 2 if start = %d and perPage = %d", start, perPage))
 	}
 }
@@ -55,10 +54,9 @@ func TestPagingOnePast(t *testing.T) {
 	var start = 31
 	var perPage = 10
 	var total = 44
-	
+
 	pageInfo1 := graphql_endpoint.FigurePaging(start, perPage, total)
-	if (pageInfo1.CurrentPage != 4) {
+	if pageInfo1.CurrentPage != 4 {
 		t.Error(fmt.Printf("should be page 4 if start = %d and perPage = %d", start, perPage))
 	}
 }
-
