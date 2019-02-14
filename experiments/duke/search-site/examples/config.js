bc17570c9b6d0358602c3a2dc252a16597f0c941
Rob Nelson <robert.nelson@duke.edu>
2019-02-14 12:06:38 -0500

    #14: get sample aggregations working locally

--- experiments/duke/search-site/examples/config.js
+++ experiments/duke/search-site/examples/config.js
@@ -1,4 +1,5 @@
-var host = 'https://elasticsearch-ads-graphql-elastic.cloud.duke.edu';
+//var host = 'https://elasticsearch-ads-graphql-elastic.cloud.duke.edu';
+var host = 'http://localhost:9200';
 
 var elasticsearch = require('elasticsearch');
 var client = new elasticsearch.Client({
--- experiments/duke/search-site/examples/search7.js
+++ experiments/duke/search-site/examples/search7.js
@@ -3,19 +3,6 @@ import { inspectAgg, error } from './search'
 
 //https://stackoverflow.com/questions/48784733/aggregation-on-keyword-field-fails
 
-/*
-       "experience_slug": {
-        "type": "text",
-        "fields": {
-          "keyword": {
-            "type": "keyword",
-            "ignore_above": 256
-          }
-        },
-        "analyzer": "default",
-        "search_analyzer": "default_search"
-      },
-      */
 // facets=aggregation, bucketing etc...
 client.search({
   //size: "0",
@@ -24,30 +11,19 @@ client.search({
   //type: 'person',
   body: {
     query: {
-      "query_string": { query: "model*" }
+      "query_string": { query: "*" }
     },
     aggs: {
-       "typeFilter": {
-           //"terms" : { "field": "keywordList.label" } 
-   
-       /*
-       "typeFilter": {
-           "terms" : { "field": "type" } 
-       */
-         
+      "types" : { "terms": {"field" : "type.label" }},
+      "keywordList": {
          "nested": {
              "path": "keywordList"
          },
          "aggs": {
-           "keywords_filter" : { "terms" : { "field": "keywordList.label.keyword" } }
+           "keyword" : { "terms" : { "field": "keywordList.label.keyword" } }
          }
-         
-      },
-      /*
-      "keywordFilter": {
-           "terms" : { "field": "keywordList" } 
       }
-      */
+      
     }
   }
 }).then(inspectAgg, error);
