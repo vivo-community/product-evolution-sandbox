cbc87999964c0339be106b57154dab0ef3586a7b
Rob Nelson <robert.nelson@duke.edu>
2019-02-21 09:51:29 -0500

    #14: add 'updates only' way to import into elastic

--- experiments/duke/elastic_import/psql/searcher.go
+++ experiments/duke/elastic_import/psql/searcher.go
@@ -5,60 +5,95 @@ import (
 	//"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
+	"time"
 )
 
-func RetrieveType(typeName string) []widgets_import.Resource {
+func RetrieveType(typeName string, updates bool) []widgets_import.Resource {
 	db := GetConnection()
 	resources := []widgets_import.Resource{}
 
-	err := db.Select(&resources, "SELECT uri, type, hash, data FROM resources WHERE type =  $1", typeName)
+	var err error
+	if updates {
+		// TODO: ideally would need to record time last run somewhere
+		yesterday := time.Now().AddDate(0, 0, -1)
+        rounded := time.Date(yesterday.Year(), yesterday.Month(), 
+		    yesterday.Day(), 0, 0, 0, 0, yesterday.Location())
+
+		sql := `SELECT uri, type, hash, data 
+		FROM resources 
+		WHERE type =  $1 and updated_at >= $2
+      `
+		err = db.Select(&resources, sql, typeName, rounded)
+	} else {
+		err = db.Select(&resources, "SELECT uri, type, hash, data FROM resources WHERE type =  $1", typeName)
+	}
+
 	if err != nil {
 		log.Fatalln(err)
 	}
 	return resources
 }
 
-func ListType(typeName string) {
+func ListType(typeName string, updates bool) {
 	db := GetConnection()
 	resources := []widgets_import.Resource{}
 
-	err := db.Select(&resources, "SELECT uri, type, hash, data FROM resources WHERE type =  $1",
-		typeName)
+	var err error
+	if updates {
+		// TODO: ideally would need to record time last run somewhere
+		yesterday := time.Now().AddDate(0, 0, -1)
+		//yesterday := time.Now()
+
+        rounded := time.Date(yesterday.Year(), yesterday.Month(), 
+		    yesterday.Day(), 0, 0, 0, 0, yesterday.Location())
+
+		sql := `SELECT uri, type, hash, data 
+		FROM resources 
+		WHERE type =  $1 and updated_at >= $2
+      `
+		err = db.Select(&resources, sql, typeName, rounded)
+	} else {
+		err = db.Select(&resources, "SELECT uri, type, hash, data FROM resources WHERE type =  $1", typeName)
+	}
+
+	//err := db.Select(&resources, "SELECT uri, type, hash, data FROM resources WHERE type =  $1",
+	//	typeName)
 	for _, element := range resources {
 		log.Println(element)
 		// element is the element from someSlice for where we are
 	}
+	log.Printf("******* count = %d ********\n", len(resources))
+
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func ListPeople() {
-	ListType("Person")
+func ListPeople(updated bool) {
+	ListType("Person", updated)
 }
 
-func ListPositions() {
-	ListType("Position")
+func ListPositions(updated bool) {
+	ListType("Position", updated)
 }
 
-func ListEducations() {
-	ListType("Education")
+func ListEducations(updated bool) {
+	ListType("Education", updated)
 }
 
-func ListGrants() {
-	ListType("Grant")
+func ListGrants(updated bool) {
+	ListType("Grant", updated)
 }
 
-func ListFundingRoles() {
-	ListType("FundingRole")
+func ListFundingRoles(updated bool) {
+	ListType("FundingRole", updated)
 }
 
-func ListPublications() {
-	ListType("Publication")
+func ListPublications(updated bool) {
+	ListType("Publication", updated)
 }
 
 // just a wrapper
-func ListAffiliations() {
-	ListType("Position")
+func ListAffiliations(updated bool) {
+	ListPositions(updated)
 }
-
